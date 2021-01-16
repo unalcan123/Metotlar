5a18cdfdb3486e687f9f1135a8e047bd7299933a
Ünal Canpolat <unal_canpolat@hotmail.com>
2021-01-16 23:30:33 +0100

    Test

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+
+            sayi2 = 65;
+
+
+            int[] sayilar1 = new int[] {10,20,30 };
+            int[] sayilar2 = new int[] {100,200,300 };
+
+            sayilar1 = sayilar2;
+
+            sayilar2[0] = 999;
+
+            int[] arr = new int[26];
+            for (int i = 0; i < 26; i++)
+                arr[i] = i;
+
+            Console.WriteLine("Array unal line : " + arr);
+
+            Console.WriteLine(sayi1);
+            Console.WriteLine(sayilar1[0]);
+
+
+
+        }
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elmasi";
+           
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakir Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine(urun.Fiyati);
+            }
+
+
+
+            Console.WriteLine("--------------metotlar------------");
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+            //sepetManager.Ekle();
+            // sepetManager.Ekle();
+
+            sepetManager.Ekle2("Armut", "yesil Armut", 12,100);
+            sepetManager.Ekle2("Elma", "yesil Elma", 12,500);
+            sepetManager.Ekle2("Karpuz", "Diyabakir karpuzu", 12,250);
+        }
+
+        
+    }
+}
