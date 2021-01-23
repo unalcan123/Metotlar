726894cafcea7367b634583baac6cee8e5f9ba77
Ünal Canpolat <unal_canpolat@hotmail.com>
2021-01-23 21:19:21 +0100

    cumartesi

---
+++ OOP3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();    //  ihtiyacKrediManager.Hesapla();
+            IKrediManager tasitKrediManager = new TasitKrediManager();       //  tasitKrediManager.Hesapla();
+            IKrediManager konutKrediManager = new KonutKrediManager();      //  konutKrediManager.Hesapla();
+                               
+            BasvuruManager basvuruManager = new BasvuruManager();
+            // basvuruManager.BasvuruYap(tasitKrediManager);     //cok onemli bir burasi.....
+
+
+            List<IKrediManager> krediler = new List<IKrediManager>(){ihtiyacKrediManager,  tasitKrediManager,konutKrediManager};
+
+            basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
---
+++ Oop2/MusteriManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+{        //is siniflari
+    class MusteriManager
+    { 
+        public void Ekle(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+        }
+    }
+}
